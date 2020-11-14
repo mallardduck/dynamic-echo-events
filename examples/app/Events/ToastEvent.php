5ca5314ef48d394088ff8d2648043be67cfc1750
MallardDuck <self@danpock.me>
2020-11-14 18:51:54 -0500

    Add more verbose docs to example event
    The example event makes things a bit more clear the idea how it should work.

--- examples/app/Events/ToastEvent.php
+++ examples/app/Events/ToastEvent.php
@@ -16,12 +16,31 @@ class ToastEvent implements ShouldBroadcastNow, ImplementsDynamicEcho
 
     public int $userId;
 
+    /**
+     * Must be a string matching toastr toast type.
+     *
+     * Options:
+     *  - info
+     *  - warning
+     *  - success
+     *  - error
+     *
+     * @var string
+     */
     public string $type;
 
+    /**
+     * A message text string for the toast notification.
+     *
+     * @var string
+     */
     public string $message;
 
     /**
-     * Create a new event instance.
+     * Create a new ToastEvent instance.
+     *
+     * The best way to do this is just using the event helper method.
+     * So something like: event(new ToastEvent('success', 'Item created successfully!'))
      *
      * @return void
      */
@@ -34,6 +53,15 @@ class ToastEvent implements ShouldBroadcastNow, ImplementsDynamicEcho
         $this->message = $message;
     }
 
+    /**
+     * Get the JS callback for this event.
+     *
+     * In this case, we're using toastr JS library to fire off a toast notification.
+     * This obviously requires loading in the library tho, so either include or compile it in.
+     * See: https://github.com/CodeSeven/toastr
+     *
+     * @return string
+     */
     public static function getEventJSCallback(): string
     {
         return <<<JSCALLBACK
