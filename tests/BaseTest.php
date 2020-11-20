f0c6aa8383fa33be4f025c6884ff0753952953ef
MallardDuck <self@danpock.me>
2020-11-19 21:55:44 -0500

    clean up base test

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -3,27 +3,14 @@
 namespace MallardDuck\DynamicEcho\Tests;
 
 use MallardDuck\DynamicEcho\DynamicEchoServiceProvider;
-use Orchestra\Testbench\Concerns\CreatesApplication;
 use Orchestra\Testbench\TestCase;
 
 abstract class BaseTest extends TestCase
 {
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        // additional setup
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             DynamicEchoServiceProvider::class
         ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        // perform environment setup
     }
 }
